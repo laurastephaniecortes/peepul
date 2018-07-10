890bbbfa61c4cb6c4612d2e587c1a92e2d463dc1
Your Name <you@example.com>
2018-07-10 17:09:23 -0400

    init

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+import (
+  "net/http"
+  "encoding/json"
+  "fmt"
+
+  "github.com/nanobox-io/golang-scribble"
+
+)
+
+type Person struct {
+    Name string
+    BodyType string
+    Age int64
+    Sex bool
+    Attractiveness float64 `validate:"max=1"`
+}
+
+type Hookup struct {
+  Top Person
+  Bottom Person
+}
+
+func getPeople(w http.ResponseWriter, r *http.Request) {
+  db, err := scribble.New("peeps.db", nil)
+  if err != nil {
+    fmt.Println("Error", err)
+  }
+  records, err := db.ReadAll("people")
+  if err != nil {
+    fmt.Println("Error", err)
+  }
+  for _, record := range records {
+
+
+  w.Write([]byte(string(record)))
+}
+}
+
+func addPerson(w http.ResponseWriter, r *http.Request) {
+  person := Person{}
+  decoder := json.NewDecoder(r.Body)
+    err := decoder.Decode(&person)
+    if err != nil {
+        panic(err)
+    }
+
+  db, err := scribble.New("peeps.db", nil)
+  if err != nil {
+    fmt.Println("Error", err)
+  }
+
+if err := db.Write("people", person.Name, person); err != nil {
+  fmt.Println("Error", err)
+}
+
+
+}
+
+
+func main() {
+  http.HandleFunc("/", getPeople)
+  http.HandleFunc("/addperson", addPerson)
+  if err := http.ListenAndServe(":8080", nil); err != nil {
+    panic(err)
+  }
+}
